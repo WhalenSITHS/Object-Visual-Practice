ecabc0cd0eb65d80eb7c319f4194bf53fe2ae2ca
Mike Whalen <Michael.whalen@sitechhs.com>
2020-07-02 08:54:12 -0400

    testing styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-console.log("connected");
+const menu = [
+  {
+    name: "Pizza Slice",
+    vegetarian: true,
+    price: 2.0,
+    img:
+      "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    inStock: true,
+  },
+  {
+    name: "Salad",
+    vegetarian: true,
+    price: 5.0,
+    img:
+      "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    inStock: true,
+  },
+];
