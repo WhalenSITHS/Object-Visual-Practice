c886cfce66b906901daea8ee4be53b31403b2dd4
Mike Whalen <Michael.whalen@sitechhs.com>
2020-07-02 09:30:11 -0400

    init function working with menu items

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -130,7 +130,52 @@ var menu = [{
   price: 5.0,
   img: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
   inStock: true
+}, {
+  name: "Soup",
+  vegetarian: true,
+  price: 3.0,
+  img: "https://images.unsplash.com/photo-1547592166-23ac45744acd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",
+  inStock: true
+}, {
+  name: "Grilled Cheese",
+  vegetarian: true,
+  price: 4,
+  img: "https://images.unsplash.com/photo-1528735602780-2552fd46c7af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80",
+  inStock: false
+}, {
+  name: "Roast Chicken",
+  vegetarian: false,
+  price: 8.0,
+  img: "https://images.unsplash.com/photo-1532550907401-a500c9a57435?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  inStock: true
+}, {
+  name: "Steak",
+  vegetarian: false,
+  price: 10.0,
+  img: "https://images.unsplash.com/photo-1574969884448-fe5bce3d0d51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80",
+  inStock: false
+}, {
+  name: "Cheeseburger",
+  vegetarian: false,
+  price: 6,
+  img: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=902&q=80",
+  inStock: true
+}, {
+  name: "Ice Cream",
+  vegetarian: false,
+  price: 3.0,
+  img: "https://images.unsplash.com/photo-1566454419290-57a64afe30ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
+  inStock: true
 }];
+
+var init = function init() {
+  var displayContainer = document.querySelector(".container");
+  var displayItems = menu.forEach(function (item) {
+    return displayContainer.insertAdjacentHTML("afterbegin", "<ul class=\"item-list\">\n      <li class=\"item-name item-value\">".concat(item.name, "</li>\n      <li class=\"item-price item-value\"> $").concat(item.price, "</li>\n      <li class=\"item-vegetarian item-value\">Vegetarian: ").concat(item.vegetarian, "</li>\n      <li class=\"item-image\">\n        <img\n          class=\"item-image\"\n          src=\"").concat(item.img, "\"\n          alt=\"\"\n        />\n      </li>\n      <li class=\"item-in-stock item-value\">In Stock: ").concat(item.inStock, "</li>\n    </ul>"));
+  });
+};
+
+init();
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -159,7 +204,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42007" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36627" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
